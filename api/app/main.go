a3b106459120de29f8a9e25ee576007191dc955f
mizuki_hashimoto <100zuha.shikitomi@gmail.com>
2023-05-04 03:35:46 +0200

    test: Add unit tests for User handlers

--- api/app/main.go
+++ api/app/main.go
@@ -13,28 +13,34 @@ var (
 	userController controllers.UserController = controllers.New(userService)
 )
 
+func GetUsersHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, userController.GetUsers())
+}
+
+func GetUserByIdHandler(ctx *gin.Context) {
+	user, err := userController.GetUserById(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"code": "PAGE_NOT_FOUND", "message": err.Error()})
+	}
+	ctx.JSON(http.StatusOK, user)
+}
+
+func CreateUserHandler(ctx *gin.Context) {
+	user, err := userController.CreateUser(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	ctx.JSON(http.StatusCreated, user)
+}
+
 func main() {
 	router := gin.Default()
 
-	router.GET("/users", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, userController.GetUsers())
-	})
+	router.GET("/users", GetUsersHandler)
 
-	router.GET("/users/:id", func(ctx *gin.Context) {
-		user, err := userController.GetUserById(ctx)
-		if err != nil {
-			ctx.JSON(http.StatusNotFound, gin.H{"code": "PAGE_NOT_FOUND", "message": err.Error()})
-		}
-		ctx.JSON(http.StatusOK, user)
-	})
+	router.GET("/users/:id", GetUserByIdHandler)
 
-	router.POST("/user", func(ctx *gin.Context) {
-		user, err := userController.CreateUser(ctx)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		}
-		ctx.JSON(http.StatusCreated, user)
-	})
+	router.POST("/user", CreateUserHandler)
 
 	router.Run("localhost:8080")
 }
--- api/app/models/user.go
+++ api/app/models/user.go
@@ -5,9 +5,9 @@ import "time"
 type User struct {
 	Id           uint      `json:"id"`
 	DisplayName  string    `json:"display_name"`
-	Username     string    `json:"username"`
-	Email        string    `json:"email"`
-	Password     string    `json:"password"`
+	Username     string    `json:"username" binding:"required"`
+	Email        string    `json:"email" binding:"required" validate:"required,email"`
+	Password     string    `json:"password" binding:"required"`
 	ProfileImage string    `json:"profile_image"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
