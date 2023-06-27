7b860a5de363b68f23c5f72e7cca442d829fd3e3
mizuki_hashimoto <100zuha.shikitomi@gmail.com>
2023-06-27 05:25:34 +0200

    chore: Setup Relay

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    relay: {
+      src: './',
+      artifactDirectory: './__generated__',
+      language: 'typescript',
+      eagerEsModules: false,
+    },
+  },
+};
 
 module.exports = nextConfig;
---
+++ frontend/relay.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  src: './',
+  language: 'typescript',
+  schema: './schema/schema.graphql',
+  artifactDirectory: './__generated__',
+  exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
+};
